a8000b81c507e382fedc033c5df9528af095c8a1
chandrakantap <pal.chandrakanta@gmail.com>
2020-07-12 08:15:13 +0530

    Initialized from start.spring.io

---
+++ src/main/java/com/cksutil/sassyboot/SassybootApplication.java
@@ -0,0 +1,13 @@
+package com.cksutil.sassyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SassybootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SassybootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cksutil/sassyboot/SassybootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cksutil.sassyboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SassybootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
